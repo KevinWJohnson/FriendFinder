164425dda791b3eeee0bbf21cafe8ead1e3bfed3
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-23 17:31:19 -0700

    Jacob Deming friend added to see if the photo whould show up on the FriendFinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -39,6 +39,22 @@ var FriendsArray = [
     ]
   },
   {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+      4,
+      2,
+      5,
+      1,
+      3,
+      2,
+      2,
+      1,
+      3,
+      2
+    ]
+  },
+  {
     "name": "Ahmed",
     "photo": "https://www.pexels.com/photo/man-young-happy-smiling-91227/",
     "scores": [
